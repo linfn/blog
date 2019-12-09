870df5109669595c668dec0e3f4f42f7c2d92f05
linfn <linfeng.lu@hotmail.com>
2019-12-10 04:03:00 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,12 @@
 # If a command fails then the deploy stops
 set -e
 
+if [ "`git status -s`" ]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
@@ -15,11 +21,19 @@ cd public
 git add .
 
 # Commit changes.
-msg="rebuilding site $(date)"
+msg="deploy to gh pages"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
 git commit -m "$msg"
 
 # Push source and build repos.
+git push origin master
+
+cd ..
+
+git add public
+
+git commit -m "$msg"
+
 git push origin master
